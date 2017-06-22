9c963c26fa64a843443035696d88d33ddf4c2e9b
Chris Hamons <chris.hamons@xamarin.com>
2017-06-22 15:06:13 -0500

    Example test showing brokeness

---
+++ tests/managed/nativeRetuns.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+#if __MACOS__
+using PlatformColor = AppKit.NSColor;
+#elif __IOS__
+using PlatformColor = UIKit.UIColor;
+#endif
+
+
+#if __MACOS__ || __IOS__
+namespace NativeReturns
+{
+	public class Colors
+	{
+		public Colors ()
+		{
+		}
+
+		public PlatformColor Color { get; }
+
+		public PlatformColor CalculateFavoriteColor ()
+		{
+			return null;
+		}
+	}
+}
+#endif
